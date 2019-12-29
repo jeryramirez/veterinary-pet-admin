3ae6d677df997fae004214f1c92c0cea2afb255a
Alexander <alexanderamirez002@gmail.com>
2019-12-29 09:37:45 -0800

    I have added the build folder to deploy

---
+++ build/precache-manifest.b33afbbb26bbe316053d423bd0e32395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe22784810cac6d2bde6ba532940c72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02d836ab85d1c5e41293",
+    "url": "/static/css/main.6c0b3faa.chunk.css"
+  },
+  {
+    "revision": "2de767588dc525abbc08",
+    "url": "/static/js/2.1d0e4da6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1d0e4da6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02d836ab85d1c5e41293",
+    "url": "/static/js/main.b41bcf0d.chunk.js"
+  },
+  {
+    "revision": "f59b4d47ef6d697185f4",
+    "url": "/static/js/runtime-main.e8a73a59.js"
+  }
+]);
